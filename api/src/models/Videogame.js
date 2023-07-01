2f8403c310cd9b60c31202807a9420ac2d1c96e9
Esteban.much <esteban.muchenvicz@gmail.com>
2023-07-01 20:02:20 -0300

    initial commit

---
+++ api/src/models/Videogame.js
@@ -0,0 +1,19 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('Videogame', {
+    //creo una id diferente al de la API
+    id:{
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    
+  });
+};
